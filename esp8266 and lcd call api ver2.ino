2092398f91c27bde938f874e2657fdb555366a71
Nevin Danielson <nevindanielson@gmail.com>
2021-12-13 20:06:52 -0600

    Update esp8266 and lcd call api ver2.ino
    if http payload error, send ""
    if "", GetPayload again

--- esp8266 and lcd call api ver2.ino
+++ esp8266 and lcd call api ver2.ino
@@ -21,120 +21,14 @@
 
 //declare global variables
 int timestamp;
-String payload;
+String payload = "";
 bool debug=false;
-bool debugGetIndex=true;
-bool debugGetString=true;
+bool debugGetIndex=false;
+bool debugGetString=false;
 
 LiquidCrystal lcd(d8,d9,d4,d5,d6,d7);
 
 ESP8266WiFiMulti WiFiMulti;
-
-
-String GetPayload (ESP8266WiFiMulti WiFiMulti)
- {
-  if (debug)  Serial.println("in GetPayload function");
-   if ((WiFiMulti.run() == WL_CONNECTED)) 
-  {
-    WiFiClient client;
-    HTTPClient http;
-    if (debug)    Serial.print("[HTTP] begin...\n");
-    if (http.begin(client, "http://api.openweathermap.org/data/2.5/weather?q=Regina&units=metric&appid=[YOUR_API_KEY")) 
-    { 
-      if (debug)    Serial.print("[HTTP] GET...\n");
-      // start connection and send HTTP header
-      int httpCode = http.GET();
-      // httpCode will be negative on error
-      if (httpCode > 0) 
-      {
-        // HTTP header has been send and Server response header has been handled
-        if (debug)    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-      }
-     // file found at server
-      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
-      {
-        if (debug)    Serial.println("returning payload");
-        return http.getString();
-      }
-    }
-  }
-}
-
-//instead of using a parser for JSON, I'm just searching for keywords in the String
-//the numbers change, but the keywords stay the same. I can find the numbers from those anchors.
-
-int ReturnDistanceFromTagOfFirstDigit(int startingPosition)
-{
-    if (debugGetIndex)  Serial.println("in ReturnIndexOfFirstDigit function");
-    if (debugGetIndex)  Serial.println(payload.charAt(startingPosition));
-    if (debugGetIndex)  Serial.println(startingPosition);
-  int currentPosition = startingPosition;
-  while (!((isDigit(payload.charAt(currentPosition)))||((payload.charAt(currentPosition)==('-'))))){
-    currentPosition++;
-    }
-  int distance = (currentPosition-startingPosition);
-    return distance;
-           
-}
-
-int ReturnDistanceFromStartOfNumber(int startingPosition)
-{
-   if (debugGetIndex)  Serial.println("in ReturnIndexOfLastDigit function");
-  int currentPosition = startingPosition+1;
-  while (isDigit(payload.charAt(currentPosition))||(payload.charAt(currentPosition)=='.')){
-    currentPosition++;
-    }
-  int distance = (currentPosition-startingPosition);
-    return distance;
-}
-
-String GetString(int indexForTagword)
-{
-    if (debugGetString)  Serial.println("in GetString function");
-    if (debugGetString)  Serial.println("staring tag starts at ");
-    if (debugGetString)  Serial.println(indexForTagword);
-
-  int distanceFromTag = ReturnDistanceFromTagOfFirstDigit(indexForTagword);
-    if (debugGetString)  Serial.println("back in GetString, now with a distance from the tag");
-    if (debugGetString)  Serial.println(distanceFromTag);
-
-  int startOfNumberIndex = (indexForTagword + distanceFromTag);
-  int distanceFromStart = ReturnDistanceFromStartOfNumber(startOfNumberIndex);
-    if (debugGetString)  Serial.println("back in GetString, now with a known index distance");
-    if (debugGetString)  Serial.println(distanceFromStart);
-  int endOfNumberIndex = (startOfNumberIndex + distanceFromStart);
-
- // now that I know the end, capture it as a String
-     if (debugGetString)    Serial.println("here is the string being returned: " );
-     if (debugGetString)    Serial.println(payload.substring(startOfNumberIndex, endOfNumberIndex));
-
-  return payload.substring(startOfNumberIndex, endOfNumberIndex);
-        
-}
-
-void DisplayLCD(String topLine, String bottomLine, String topLine2, String bottomLine2)
-{ 
-    if (debug)  Serial.println("in DisplayLCD function");      
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("min: " + topLine);
-  lcd.setCursor(0, 1);
-  lcd.print("max: " + bottomLine);
-  delay (2500);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("temp: " + topLine2);
-  lcd.setCursor(0, 1);
-  lcd.print("feels like: " + bottomLine2);
-}
-
-//test "feels_like" and turn on warning LED
-void ToggleColdWarning (String feelString)
-{
- if (feelString.toDouble()<=-25)   digitalWrite (3, HIGH);
- else                              digitalWrite(3, LOW);
-}
-        
 
 void setup() {
   Serial.begin(115200);
@@ -144,23 +38,13 @@ void setup() {
   lcd.begin(16, 2);
   timestamp = millis();
   for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
+    if (debug)  Serial.printf("[SETUP] WAIT %d...\n", t);
     Serial.flush();
     delay(1000);
   }
   WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("[YOUR_SSID]", "[YOUR_PASSWORD]");
-  payload = GetPayload(WiFiMulti);
-    if (debug)  Serial.println("done GetPayload function. Printing...");
-    if (debug)  Serial.println(payload);
+  WiFiMulti.addAP("SASKTEL050", "3065357624");
   pinMode(3, OUTPUT);
-  String tempAsAString=GetString(payload.indexOf("temp"));
-  String feelAsAString=GetString(payload.indexOf("feels_like"));
-  String maxAsAString=GetString(payload.indexOf("temp_max"));
-  String minAsAString=GetString(payload.indexOf("temp_min"));
- 
-  DisplayLCD(minAsAString, maxAsAString, tempAsAString, feelAsAString);
-  delay(2500);
 }
 
 void loop() 
@@ -170,15 +54,96 @@ void loop()
      payload = GetPayload(WiFiMulti);
      timestamp=millis();
   }
- 
+
+  if (payload == "") {
+     payload = GetPayload(WiFiMulti);
+     timestamp=millis();
+  }
   int indexForTagword = (payload.indexOf("temp"));
   String tempAsAString=GetString(payload.indexOf("temp"));
   String feelAsAString=GetString(payload.indexOf("feels_like"));
   String maxAsAString=GetString(payload.indexOf("temp_max"));
   String minAsAString=GetString(payload.indexOf("temp_min"));
-     
   DisplayLCD(minAsAString, maxAsAString, tempAsAString, feelAsAString);
   ToggleColdWarning (feelAsAString);
-  
-  delay(2500);
+  delay(3000);
+}
+
+String GetPayload (ESP8266WiFiMulti WiFiMulti)
+ {
+  if ((WiFiMulti.run() == WL_CONNECTED)) 
+  {
+    WiFiClient client;
+    HTTPClient http;
+    if (debug)    Serial.print("[HTTP] begin...\n");
+    if (http.begin(client, "http://api.openweathermap.org/data/2.5/weather?q=Regina&units=metric&appid=54cc82d379192b5fd110a47129f46a56")) 
+    { 
+        if (debug)    Serial.print("[HTTP] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = http.GET();
+      // httpCode will be negative on error
+      if (httpCode > 0) 
+      {
+        // HTTP header has been send and Server response header has been handled
+          if (debug)    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      }
+     // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
+      {
+        return http.getString();
+      }
+    }
+  }
+  return "";
+}
+
+int ReturnDistanceFromTagOfFirstDigit(int startingPosition)
+{
+  int currentPosition = startingPosition;
+  while (!((isDigit(payload.charAt(currentPosition)))||((payload.charAt(currentPosition)==('-'))))){
+    currentPosition++;
+    }
+  int distance = (currentPosition-startingPosition);
+  return distance;
+}
+
+int ReturnDistanceFromStartOfNumber(int startingPosition)
+{
+  int currentPosition = startingPosition+1;
+  while (isDigit(payload.charAt(currentPosition))||(payload.charAt(currentPosition)=='.')){
+    currentPosition++;
+    }
+  int distance = (currentPosition-startingPosition);
+  return distance;
+}
+
+String GetString(int indexForTagword)
+{
+  int distanceFromTag = ReturnDistanceFromTagOfFirstDigit(indexForTagword);
+  int startOfNumberIndex = (indexForTagword + distanceFromTag);
+  int distanceFromStart = ReturnDistanceFromStartOfNumber(startOfNumberIndex);
+  int endOfNumberIndex = (startOfNumberIndex + distanceFromStart);
+  return payload.substring(startOfNumberIndex, endOfNumberIndex);
+}
+
+void DisplayLCD(String minLine, String maxLine, String tempLine, String feelLine)
+{ 
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("min: " + minLine);
+  lcd.setCursor(0, 1);
+  lcd.print(" max: " + maxLine);
+  delay (3000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("temp: " + tempLine);
+  lcd.setCursor(0, 1);
+  lcd.print(" feels: " + feelLine);
+}
+
+//test "feels_like" and turn on warning LED
+void ToggleColdWarning (String feelString)
+{
+  if (feelString.toDouble()<=-25)   digitalWrite (3, HIGH);
+  else                              digitalWrite(3, LOW);
 }
